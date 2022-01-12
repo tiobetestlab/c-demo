789b36ccae5d77e9be9cc2739aa1839b68d828e8
marvinwener <45437696+marvinwener@users.noreply.github.com>
2022-01-12 14:04:50 +0100

    Update drte.c

--- dRTE/drte.c
+++ dRTE/drte.c
@@ -14,7 +14,7 @@ extern void MMI_KL15Change_Callback(void);
 /* DATA DEFINITION */
 /*******************/
 static uint16   EngineSpeed	    = 5000u;
-static int8     EngineTemp      = 800u;
+static int8     EngineTemp      = 88u;
 static t_Engine EngineStruct    = {5000u, 80u};
 static uint16   XmsnOutputSpeed = 3000u;
 static uint16   XmsnInputSpeed  = 5000u;
