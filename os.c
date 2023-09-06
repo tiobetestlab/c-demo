cb177a4023f4f871079991aea55d408ccfe49501
peeters <116091669+peeters-tiobe@users.noreply.github.com>
2023-09-06 14:50:58 +0200

    Update os.c

--- os.c
+++ os.c
@@ -5,22 +5,14 @@ include guard */
 //#include "std_types.h"
 //Comment to create a pull request
 
-/* Introduced violations */
-static uint16   Violation1      = 5000u;
-static uint16   Violation2      = 5000u;
-static uint16   Violation3      = 5000u;
-static uint16   Violation4      = 5000u;
-static uint16   Violation5      = 5000u;
-static uint16   Violation6      = 5000u;
-
 extern const t_dRteEvent taskTable_TASK[];
 extern void drteRunnableExec(t_dRteEvent runnableFunc[], t_drte_event event);
 void task(void);
 
 void task(void)
 {
-     uint8 event = 666;
-     uint8 task  = 666;
+     uint8 event = 0;
+     uint8 task  = 0;
      
      DRTE_GET_EVENT(task, &event);
      DRTE_CLR_EVENT(task, event);
