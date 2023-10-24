46eab619db15a49868c66c6e857298d3c2b1d108
peeters <116091669+peeters-tiobe@users.noreply.github.com>
2023-10-24 15:30:13 +0200

    Update os.c

--- os.c
+++ os.c
@@ -10,8 +10,8 @@ void task(void);
 
 void task(void)
 {
-     uint32 event    = 0;
-     uint8  task     = 0;
+     uint32 event    = 666;
+     uint8  task     = 666;
      
      DRTE_GET_EVENT(task, &event);
      DRTE_CLR_EVENT(task, event);
